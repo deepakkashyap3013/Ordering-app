c15cf9907207c6b80a01750e1655c3b13afdfb88
deepakkashyap3013 <deepaklining@gmail.com>
2022-06-27 00:03:40 +0530

    API Project commit

---
+++ apps/orders/src/orders.service.ts
@@ -0,0 +1,39 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { lastValueFrom } from 'rxjs';
+import { BILLING_SERVICE } from './constants/services';
+import { CreateOrderRequest } from './dto';
+import { OrderRepository } from './orders.repository';
+
+@Injectable()
+export class OrdersService {
+  
+  constructor(
+    private readonly orderRepository: OrderRepository,
+    @Inject(BILLING_SERVICE) private billingClient:ClientProxy
+  ) { }
+
+
+  async createOrder(request: CreateOrderRequest,authentication:string) {
+    const session = await this.orderRepository.startTransaction();
+    try {
+      const order = await this.orderRepository.create(request,{session});
+      await lastValueFrom(
+        this.billingClient.emit('order_created', {
+          request,
+          Authentication:authentication
+        })
+      )
+      await session.commitTransaction();
+      return order;
+    } catch (err) {
+      session.abortTransaction();
+      throw err;
+    }
+ 
+  }
+
+  async getOrders() {
+    return this.orderRepository.find({});
+  }
+}
